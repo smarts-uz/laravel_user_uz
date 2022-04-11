efab2b45b3e8a1fe1c67dd7c893d39ee3da92972
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-04-11 16:31:20 +0500

    paxoje zadaniya bug fix

--- resources/views/task/detailed-tasks.blade.php
+++ resources/views/task/detailed-tasks.blade.php
@@ -552,17 +552,17 @@
                                                         <img src="" class="text-2xl float-left text-blue-400 sm:mr-4 mr-3 h-14 w-14 bg-blue-200 p-2 rounded-xl"/>
                                                     </div>
                                                     <div class="w-5/6">
-                                                        <a href="" class="sm:text-lg text-base font-semibold text-blue-500 hover:text-red-600"></a>
-                                                        <p class="text-sm  location "></p>
-                                                        <p class="text-sm my-0.5"></p>
-                                                        <p class="text-sm "></p>
+                                                        <a href="#" class="sm:text-lg text-base font-semibold text-blue-500 hover:text-red-600">Dizayner Kerak</a>
+                                                        <p class="text-sm">Узбекистан, Ташкент</p>
+                                                        <p class="text-sm my-0.5">Начать 7-апрель 5:15</p>
+                                                        <p class="text-sm ">Оплата через карту</p>
                                                     </div>
                                                 </div>
-                                                <div class="sm:col-span-2 col-span-5 sm:text-right text-left sm:ml-0 ml-16" id="about">
-                                                    <p  class="sm:text-lg text-sm font-semibold text-gray-700"></p>
-                                                    <span  class="text-sm sm:mt-5 sm:mt-1 mt-0"></span>
-                                                    <p class="text-sm sm:mt-1 mt-0"></p>
-                                                    <a href="/performers/` + data.userid + `" class="text-sm sm:mt-1 mt-0 hover:text-red-500 border-b-2 border-gray-500 hover:border-red-500"></a>
+                                                <div class="sm:col-span-2 col-span-5 sm:text-right text-left sm:ml-0 ml-16">
+                                                    <p  class="sm:text-lg text-sm font-semibold text-gray-700">до 835000 сум</p>
+                                                    <span  class="text-sm sm:mt-5 sm:mt-1 mt-0">Откликов - 0</span>
+                                                    <p class="text-sm sm:mt-1 mt-0">Верстка</p>
+                                                    <a href="#" class="text-sm sm:mt-1 mt-0 hover:text-red-500 border-b-2 border-gray-500 hover:border-red-500">akki jura</a>
                                                 </div>
                                             </div>
                                        </div>
