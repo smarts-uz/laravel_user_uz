193e14a90860d01d7f1f24e523cc65ce961d687f
Gitea <gitea@fake.local>
2022-04-19 21:47:44 +0500

    133333

---
+++ app/Item/PerformerAppItem.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ *
+ * Author:  Asror Zakirov
+ * https://www.linkedin.com/in/asror-zakirov
+ * https://github.com/asror-z
+ *
+ */
+
+namespace App\Item;
+
+
+use App\Models\Task;
+use App\Models\User;
+use TCG\Voyager\Models\Category;
+
+class PerformerAppItem
+{
+    public $tasks;
+    public $categories;
+    public $categories2;
+    public $users;
+    public $task_count;
+    public $about;
+
+}
---
+++ app/Item/PerformerUserItem.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *
+ *
+ * Author:  Asror Zakirov
+ * https://www.linkedin.com/in/asror-zakirov
+ * https://github.com/asror-z
+ *
+ */
+
+namespace App\Item;
+
+
+use App\Models\User;
+
+class PerformerUserItem
+{
+    public $task_count;
+    public $about;
+    public $reviews;
+    public $portfolios;
+
+}
