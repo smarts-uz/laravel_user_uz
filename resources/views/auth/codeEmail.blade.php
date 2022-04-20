66d495133e86735d28dcd06346036c622a08fdb7
avacoderuz <avazbeknematov01@gmail.com>
2022-04-20 20:22:37 +0500

    zabili parol by email

---
+++ resources/views/auth/codeEmail.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+    <div class="mt-3 text-center text-base">
+        <div class="mx-auto items-center justify-center w-full">
+            <h3 class="font-bold text-3xl block mt-8 mb-4">
+                {{__('Восстановление пароля')}}
+            </h3>
+            <p class="font-medium text-lg mt-3 mb-6">
+                Выберите подходящий способ:
+            </p>
+        </div>
+        <div class="mx-auto flex items-center justify-center w-full">
+        </div>
+        <form action="{{route('user.reset_code')}}" method="POST">
+            @csrf
+            <div>
+                <div class="mb-4">
+                    <label class="block text-gray-500 text-sm mb-1" for="phone_number">
+                        Введите смс-код
+                    </label>
+                    <input type="text" onfocus="onfocus" onkeypress='validate(event)'
+                           id="phone_number" name="code"
+                           class="shadow focus:outline-none focus:border-yellow-500 appearance-none border border-slate-300 rounded w-80 py-2 px-3 text-gray-700 mb-3 leading-tight ">
+                    <br>
+                    @error('code')
+                        <span class="text-red-500" >{{ $message  }}</span>
+                    @enderror
+                    @if(session()->has('error'))
+                        <span class="text-red-500">{{ session('error')  }}</span>
+                    @endif
+                </div>
+            </div>
+            <button type="submit"
+                    class="w-80 h-12 rounded-lg bg-green-500 text-gray-200 uppercase font-semibold hover:bg-green-500 text-gray-100 transition mb-4">
+                {{__('Отправить')}}
+            </button>
+        </form>
+    </div>
+@endsection
+
+
+
+@section("javasript")
+
+
+
+@endsection
--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -13,32 +13,46 @@
 
         <!-- Tabs -->
         <div class="mx-auto my-8">
-            <ul id="tabs" class="nav nav-tabs flex  text-center flex-wrap list-none border-b-0 pl-0 mb-2 justify-center">
-                <li class="bg-white text-xl px-12 text-gray-800 font-semibold hover:bg-gray-200 py-2 text-yellow-500 border-b-2 border-yellow-500"><a id="default-tab" href="#first">ЭЛ. ПОЧТА</a></li>
-                <li class="px-12 text-xl text-gray-800 hover:bg-gray-200 font-semibold py-2"><a href="#second">СМС</a></li>
+            <ul id="tabs"
+                class="nav nav-tabs flex  text-center flex-wrap list-none border-b-0 pl-0 mb-2 justify-center">
+                <li class="bg-white text-xl px-12 text-gray-800 font-semibold hover:bg-gray-200 py-2 text-yellow-500 border-b-2 border-yellow-500">
+                    <a id="default-tab" href="#first">ЭЛ. ПОЧТА</a></li>
+                <li class="px-12 text-xl text-gray-800 hover:bg-gray-200 font-semibold py-2"><a href="#second">СМС</a>
+                </li>
             </ul>
         </div>
 
         <!-- Tab Contents -->
         <div id="tab-contents" class="flex justify-center">
             <div id="first" class="p-2">
-                <div class="mx-auto flex items-center justify-center w-full">
-                    <p class="mb-4">
-                        Укажите почту, привязанную к вашей <br> учетной записи. Мы отправим письмо <br> для восстановления пароля.
-                    </p>
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-500 text-sm mb-1" for="phone_number">
-                        <span>{{__('Электронная почта')}}</span>
-                    </label>
-                    <input type="text" placeholder=""
-                        value=""
-                        id="email"
-                        class="shadow appearance-none border focus:outline-none focus:border-yellow-500 rounded w-80 py-2 px-3 text-gray-700 mb-3 leading-tight">
-                </div>
-                <button type="button" class="w-80 h-12 rounded-lg bg-green-500 text-gray-200 uppercase   font-semibold hover:bg-green-500 text-gray-100 transition mb-4">
-                    {{__('Отправить')}}
-                </button>
+                <form action="{{route('user.reset_submit')}}" method="POST">
+                    @csrf
+                    <div class="mx-auto flex items-center justify-center w-full">
+                        <p class="mb-4">
+                            Укажите почту, привязанную к вашей <br> учетной записи. Мы отправим письмо <br> для
+                            восстановления пароля.
+                        </p>
+                    </div>
+                    <div class="mb-4">
+                        <label class="block text-gray-500 text-sm mb-1" for="phone_number">
+                            <span>{{__('Электронная почта')}}</span>
+                        </label>
+                        <input type="text" placeholder="Your email"
+                               value="{{ old('email') }}" name="email"
+                               id="email"
+                               class="shadow appearance-none border focus:outline-none focus:border-yellow-500 rounded w-80 py-2 px-3 text-gray-700 mb-3 leading-tight">
+                        @if(session()->has('message'))
+                            <p class="text-red-500">{{session('message')}}</p>
+                        @endif
+                        @error('email')
+                              <p class="text-red-500">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <button type="submit"
+                            class="w-80 h-12 rounded-lg bg-green-500 text-gray-200 uppercase   font-semibold hover:bg-green-500 text-gray-100 transition mb-4">
+                        {{__('Отправить')}}
+                    </button>
+                </form>
             </div>
             <div id="second" class="p-2">
                 <div class="mx-auto flex items-center justify-center w-full">
@@ -54,19 +68,19 @@
                                 <span>{{__('Телефон немер')}}</span>
                             </label>
                             <input type="text" placeholder="+998"
-                                value="+998{{ request()->input('phone_number', old('phone_number')) }}"
-                                id="phone_number"
-                                class="shadow appearance-none border focus:outline-none focus:border-yellow-500 rounded w-80 py-2 px-3 text-gray-700 mb-3 leading-tight">
+                                   value="+998{{ request()->input('phone_number', old('phone_number')) }}"
+                                   id="phone_number"
+                                   class="shadow appearance-none border focus:outline-none focus:border-yellow-500 rounded w-80 py-2 px-3 text-gray-700 mb-3 leading-tight">
                             <br>
-        
-        
+
+
                             @if(session()->has('message'))
                                 <p class="text-red-500">{{session('message')}}</p>
                             @endif
-        
+
                             <input type="hidden" name="phone_number" id="phone">
                             @error('phone_number')
-                                <span class="text-danger" style="color: red">{{ $message  }}</span>
+                            <span class="text-danger" style="color: red">{{ $message  }}</span>
                             @enderror
                         </div>
                     </div>
@@ -78,16 +92,6 @@
             </div>
         </div>
 
-        
-   
-
-
-
-
-
-
-
-
 
     </div>
     <script>
@@ -95,28 +99,28 @@
         let tabsContainer = document.querySelector("#tabs");
         let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
         console.log(tabTogglers);
-        tabTogglers.forEach(function(toggler) {
-        toggler.addEventListener("click", function(e) {
-        e.preventDefault();
-        let tabName = this.getAttribute("href");
-        let tabContents = document.querySelector("#tab-contents");
-        for (let i = 0; i < tabContents.children.length; i++) {
-        tabTogglers[i].parentElement.classList.remove(  "text-yellow-500","border-b-2","border-yellow-500");
-        tabContents.children[i].classList.remove("hidden");
-        if ("#" + tabContents.children[i].id === tabName) {
-            continue;
-        }
-        tabContents.children[i].classList.add("hidden");
-        }
-        e.target.parentElement.classList.add( "text-yellow-500","border-b-2","border-yellow-500");
-        });
+        tabTogglers.forEach(function (toggler) {
+            toggler.addEventListener("click", function (e) {
+                e.preventDefault();
+                let tabName = this.getAttribute("href");
+                let tabContents = document.querySelector("#tab-contents");
+                for (let i = 0; i < tabContents.children.length; i++) {
+                    tabTogglers[i].parentElement.classList.remove("text-yellow-500", "border-b-2", "border-yellow-500");
+                    tabContents.children[i].classList.remove("hidden");
+                    if ("#" + tabContents.children[i].id === tabName) {
+                        continue;
+                    }
+                    tabContents.children[i].classList.add("hidden");
+                }
+                e.target.parentElement.classList.add("text-yellow-500", "border-b-2", "border-yellow-500");
+            });
         });
         $('#second').addClass('hidden');
-        $('#second').click(function(){
+        $('#second').click(function () {
             $(this).removeClass('hidden');
         })
         //tab content script end
     </script>
-     <script src='https://unpkg.com/imask'></script>
-     <script src="{{ asset('js/auth/reset.js') }}"></script>
+    <script src='https://unpkg.com/imask'></script>
+    <script src="{{ asset('js/auth/reset.js') }}"></script>
 @endsection
