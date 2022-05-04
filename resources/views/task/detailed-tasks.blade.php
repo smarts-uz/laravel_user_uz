7add95993310e48365bcd55137e4168b6f16792e
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-05-04 17:30:12 +0500

    bug fix

--- resources/views/task/detailed-tasks.blade.php
+++ resources/views/task/detailed-tasks.blade.php
@@ -270,6 +270,7 @@
                                                     type="submit">
                                                     {{__('Задание выполнено')}}
                                                 </button>
+
                                                 <button
                                                     class="not_done  sm:w-2/5 w-9/12 text-lg font-semibold bg-red-500 text-white hover:bg-red-400 px-5 ml-6 pt-2 pb-3 rounded-lg transition-all duration-300 m-2"
                                                     type="button">
@@ -969,12 +970,5 @@
             @endif
         @endforeach
     </div>
-    @if(auth()->user()->role_id == 2 || $selected->performer->role_id == 2 || $response->performer->role_id == 2)
-        <script>
-            if ($('.tooltip-2').length === 0) {
-                $("<div data-tooltip-target='tooltip-animation_2' class='mx-1 tooltip-2' ><img src='{{ asset("images/best_gray.png") }}'alt='' class='w-10'><div id='tooltip-animation_2' role='tooltip' class='inline-block  sm:w-2/12 w-1/2 absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700'><p class='text-center'>{{__('Невходит в ТОП-20 всех исполнителей User.uz')}}</p><div class='tooltip-arrow' data-popper-arrow></div> </div></div>").insertAfter($(".tooltip-1"));     
-            }
-        </script>
-    @endif
 @endsection
 
