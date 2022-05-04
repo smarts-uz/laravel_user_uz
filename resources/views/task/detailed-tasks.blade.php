841cddf7d1436f808d6fc12ab7f7c783db957267
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-05-04 16:38:38 +0500

    detailed task

--- resources/views/task/detailed-tasks.blade.php
+++ resources/views/task/detailed-tasks.blade.php
@@ -362,7 +362,7 @@
                                         @endif
                                     @endforeach
                                     <div data-tooltip-target="tooltip-animation_3" class="mx-1">
-                                        @if(auth()->user()->tasks()->count() >= 50)
+                                        @if( (auth()->user()->review_good) + (auth()->user()->review_bad) >= 50)
                                             <img src="{{ asset('images/50.png') }}" alt="" class="w-10 mt-1">
                                         @else
                                             <img src="{{ asset('images/50_gray.png') }}" alt="" class="w-10 mt-1">
@@ -498,7 +498,7 @@
                                             @endif
                                         @endforeach
                                         <div data-tooltip-target="tooltip-animation_3" class="mx-1">
-                                            @if($selected->performer->tasks()->count() >= 50)
+                                            @if( ($selected->performer->review_good) + ($selected->performer->review_bad) >= 50)
                                                 <img src="{{ asset('images/50.png') }}" alt="" class="w-10 mt-1">
                                             @else
                                                 <img src="{{ asset('images/50_gray.png') }}" alt="" class="w-10 mt-1">
@@ -646,7 +646,7 @@
                                                 @endif
                                             @endforeach
                                             <div data-tooltip-target="tooltip-animation_3" class="mx-1">
-                                                @if($response->performer->tasks()->count() >= 50)
+                                                @if( ($response->performer->review_good) + ($response->performer->review_bad) >= 50)
                                                     <img src="{{ asset('images/50.png') }}" alt="" class="w-10 mt-1">
                                                 @else
                                                     <img src="{{ asset('images/50_gray.png') }}" alt="" class="w-10 mt-1">
@@ -969,6 +969,12 @@
             @endif
         @endforeach
     </div>
-
+    @if(auth()->user()->role_id == 2 || $selected->performer->role_id == 2 || $response->performer->role_id == 2)
+        <script>
+            if ($('.tooltip-2').length === 0) {
+                $("<div data-tooltip-target='tooltip-animation_2' class='mx-1 tooltip-2' ><img src='{{ asset("images/best_gray.png") }}'alt='' class='w-10'><div id='tooltip-animation_2' role='tooltip' class='inline-block  sm:w-2/12 w-1/2 absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700'><p class='text-center'>{{__('Невходит в ТОП-20 всех исполнителей User.uz')}}</p><div class='tooltip-arrow' data-popper-arrow></div> </div></div>").insertAfter($(".tooltip-1"));     
+            }
+        </script>
+    @endif
 @endsection
 
