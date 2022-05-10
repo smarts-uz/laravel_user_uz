19e4e20de7046cb58fe24de87747cb9a67752257
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-05-10 17:01:46 +0500

    detailed task components

---
+++ resources/views/detailed_task/detailed_buttons.blade.php
@@ -0,0 +1,88 @@
+<div class="w-full flex flex-col sm:flex-row sm:p-6 p-2">
+    <div class="w-full text-center">
+        @auth
+            @if(getAuthUserBalance() >= setting('admin.pullik_otklik'))
+                @if($task->user_id != auth()->id() && $task->status < 3 && !$auth_response)
+                    <button
+                        class="sm:w-4/5 w-full font-sans text-lg pay font-semibold bg-green-500 text-white hover:bg-green-600 px-8 pt-1 pb-2 mt-6 rounded-lg transition-all duration-300"
+                        id="btn1"
+                        type="button"
+                        data-modal-toggle="authentication-modal">
+                        {{__('Откликнуться за ')}} {{setting('admin.pullik_otklik')}} UZS<br>
+                        <span class="text-sm">
+                            {{__('и отправить контакты заказчику')}}<br>
+                        </span>
+                    </button>
+                    <button
+                        class="sm:w-4/5 w-full font-sans text-lg font-semibold bg-yellow-500 text-white hover:bg-yellow-600 px-8 pt-1 pb-2 mt-6 rounded-lg transition-all duration-300"
+                        id="btn2"
+                        type="button"
+                        data-modal-toggle="authentication-modal">
+                        {{__('Откликнуться на задание бесплатно')}}<br>
+                        <span class="text-sm">
+                            {{__('отклик - 0 UZS, контакт с заказчиком - ')}} {{setting('admin.bepul_otklik')}} UZS
+                        </span>
+                    </button>
+                @endif
+            @elseif(getAuthUserBalance() < setting('admin.pullik_otklik') )
+                @if($task->user_id != auth()->id() && $task->status < 3 && !$auth_response)
+                    <a class="open-modal"
+                       data-modal="#modal1">
+                       <button
+                        class="sm:w-4/5 w-full font-sans text-lg pay font-semibold bg-green-500 text-white hover:bg-green-600 px-8 pt-1 pb-2 mt-6 rounded-lg transition-all duration-300">
+                        {{__('Откликнуться за')}}{{setting('admin.pullik_otklik')}} UZS<br>
+                        <span class="text-sm">
+                            {{__('и отправить контакты заказчику')}}<br>
+                        </span>
+                    </button>
+                    </a>
+                    <a class="open-modal"
+                       data-modal="#modal1">
+                       <button
+                        class="sm:w-4/5 w-full font-sans text-lg font-semibold bg-yellow-500 text-white hover:bg-yellow-600 px-8 pt-1 pb-2 mt-6 rounded-lg transition-all duration-300">
+                        {{__('Откликнуться на задание бесплатно')}}<br>
+                        <span class="text-sm">
+                            {{__('отклик - 0 UZS, контакт с заказчиком - ')}} {{setting('admin.bepul_otklik')}} UZS
+                        </span>
+                    </button>
+                    </a>
+                @endif
+            @endif
+        @else
+            <a href="/login">
+                <button
+                    class="sm:w-4/5 w-full mx-auto font-sans mt-8 text-lg  font-semibold bg-yellow-500 text-white hover:bg-orange-500 px-10 py-4 rounded-lg">
+                    {{__('Откликнуться на это задание')}}
+                </button>
+            </a>
+        @endauth
+        @auth
+            @if ($task->performer_id == auth()->user()->id || $task->user_id == auth()->user()->id)
+                <button id="sendbutton"
+                        class="font-sans w-full text-lg font-semibold bg-green-500 hidden text-white hover:bg-green-400 px-12 ml-6 pt-2 pb-3 rounded-lg transition-all duration-300 m-2"
+                        type="button">
+                    {{__('Оставить отзыв')}}
+                </button>
+
+                @if($task->status == 3 && $task->user_id == auth()->user()->id)
+
+
+                    @if(!$review)
+                        <button
+                            id="modal-open-id5"
+                            class="not_done  sm:w-2/5 w-9/12 text-lg font-semibold bg-green-500 text-white hover:bg-green-400 px-5 ml-6 pt-2 pb-3 rounded-lg transition-all duration-300 m-2"
+                            type="submit">
+                            {{__('Задание выполнено')}}
+                        </button>
+
+                        <button
+                            class="not_done  sm:w-2/5 w-9/12 text-lg font-semibold bg-red-500 text-white hover:bg-red-400 px-5 ml-6 pt-2 pb-3 rounded-lg transition-all duration-300 m-2"
+                            type="button">
+                            {{__('Задание не выполнено')}}
+                        </button>
+                    @endif
+                @endif
+            @endif
+        @endauth
+    </div>
+</div>
---
+++ resources/views/detailed_task/detailed_right.blade.php
@@ -0,0 +1,55 @@
+<div class="lg:w-3/12 w-full mt-8 lg:ml-8 ml-0">
+    <div class="mb-10">
+        <h1 class="text-xl font-medium mb-4">{{__('Задание')}} № {{$task->id}}</h1>
+        <div>
+            <button onclick="toggleModal44()"
+                    class="px-3 py-3 border border-3 ml-4 rounded-md border-gray-500 hover:border-gray-400">
+                <i class="fas fa-share-alt"></i>
+            </button>
+            @if (Auth::check())
+                <button onclick="toggleModal45()"
+                        class="px-3 py-3 border border-3 ml-4 rounded-md border-gray-500 hover:border-gray-400">
+                    <i class="far fa-flag"></i>
+                </button>
+            @else
+                <button
+                    class="px-3 py-3 border border-3 ml-4 rounded-md border-gray-400">
+                    <i class="far fa-flag"></i>
+                </button>
+            @endif
+        </div>
+    </div>
+    <h1 class="text-lg">{{__('Заказчик этого задания')}}</h1>
+    <div class="flex flex-col mt-4">
+        <div class="mb-4">
+            <img class="border-2 border-radius-500 border-gray-400 w-32 h-32 rounded-lg" alt="#"
+                 src="@if ($task->user->avatar == ''){{ asset("storage/images/default.png") }}
+                 @else{{asset("storage/{$task->user->avatar}") }}" @endif
+            >
+        </div>
+        <div class="">
+            @if (Auth::check() && Auth::user()->id == $task->user->id)
+                <a href="/profile"
+                   class="text-2xl text-blue-500 hover:text-red-500">{{$task->user->name ?? $task->user_name}}
+                </a>
+            @else
+                <a href="/performers/{{$task->user->id}}"
+                   class="text-2xl text-blue-500 hover:text-red-500">{{$task->user->name ?? $task->user_name}}
+                </a>
+            @endif
+
+            <br>
+            <a class="text-xl text-gray-500">
+                @if($task->user->age != "")
+                    <p class="inline-block text-m mr-2">
+                        {{$task->user->age}}
+                        @if($task->user->age>20 && $task->user->age%10==1) {{__('год')}}
+                        @elseif ($task->user->age>20 && ($task->user->age%10==2 || $task->user->age%10==3 || $task->user->age%10==1)) {{__('года')}}
+                        @else {{__('лет')}}
+                        @endif
+                    </p>
+                @endif
+            </a>
+        </div>
+    </div>
+</div>
