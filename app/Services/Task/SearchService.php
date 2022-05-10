7821b476a9b88391760456e197eb28054a86f160
Gitea <gitea@fake.local>
2022-05-10 14:40:07 +0500

    Key ID

--- app/Services/Task/SearchService.php
+++ app/Services/Task/SearchService.php
@@ -14,22 +14,24 @@ use Illuminate\Support\Facades\DB;
 
 class SearchService
 {
-    public function ajaxReq(){
+    public function ajaxReq()
+    {
         $tasks = Task::whereIn('status', [1, 2])
             ->orderBy('id', 'desc')
             ->join('users', 'tasks.user_id', '=', 'users.id')
             ->join('categories', 'tasks.category_id', '=', 'categories.id')
-            ->select('tasks.id', 'tasks.name', 'tasks.address','tasks.date_type', 'tasks.start_date', 'tasks.end_date', 'tasks.budget', 'tasks.category_id', 'tasks.status', 'tasks.oplata', 'tasks.coordinates', 'users.name as user_name', 'users.id as userid', 'categories.name as category_name', 'categories.ico as icon')
-            ->get()->load(['responses','addresses']);
+            ->select('tasks.id', 'tasks.name', 'tasks.address', 'tasks.date_type', 'tasks.start_date', 'tasks.end_date', 'tasks.budget', 'tasks.category_id', 'tasks.status', 'tasks.oplata', 'tasks.coordinates', 'users.name as user_name', 'users.id as userid', 'categories.name as category_name', 'categories.ico as icon')
+            ->get()->load(['responses', 'addresses']);
         return $tasks->all();
     }
 
-    public function comlianse_saveS($request){
+    public function comlianse_saveS($request)
+    {
         $comp = new Compliance();
-        $comp->compliance_type_id=$request->input('c_type');
-        $comp->text=$request->input('c_text');
-        $comp->user_id=$request->input('userId');
-        $comp->task_id=$request->input('taskId');
+        $comp->compliance_type_id = $request->input('c_type');
+        $comp->text = $request->input('c_text');
+        $comp->user_id = $request->input('userId');
+        $comp->task_id = $request->input('taskId');
         $comp->save();
     }
 
@@ -44,14 +46,26 @@ class SearchService
         $item->addresses = $task->addresses;
         $item->about = User::where('role_id', 2)->orderBy('reviews', 'desc')->take(20)->get();
         $item->respons_reviews = Review::all();
-        $item->review_description = Review::where('task_id',$task)->first();
+        $item->review_description = Review::where('task_id', $task)->first();
         return $item;
     }
 
-    public function search_new_service($arr_check): SearchNewItem
+    public function search_new_service($arr_check, $filter = '', $suggest = ''): SearchNewItem
     {
+
+        $users = User::all()->keyBy('id');
+        $categories = Category::all()->keyBy('id');
+
         $item = new SearchNewItem();
-        $item->tasks = DB::table('tasks')->whereIn('status', [1, 2])->whereIn('category_id', $arr_check)->get();
+        $tasks = DB::table('tasks')->whereIn('status', [1, 2])->whereIn('category_id', $arr_check)->get()->keyBy('id');
+
+        foreach ($tasks as $task) {
+            $taskNew = $task;
+            $taskNew->user_id = $users->get($task->user_id)->name;
+            $taskNew->category_id = $categories->get($task->category_id)->name;
+            $item->tasks[] = $taskNew;
+        }
+
         return $item;
     }
 }
