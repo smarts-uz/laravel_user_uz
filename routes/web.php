040d6b4a9a2b20cfa1bdd6ef698013850f01283b
Gitea <gitea@fake.local>
2022-05-11 19:42:24 +0500

    11111

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,8 @@ use App\Http\Controllers\ClickuzController;
 
 
 Route::get('/search_new', [SearchTaskController::class, 'search_new'])->name('searchTask.search_new');
+
+
 Route::post('search_new2', [SearchTaskController::class, 'search_new2'])->name('searchTask.search_new2');
 Route::any('/paynet', function () {
     (new Goodoneuz\PayUz\PayUz)->driver('paynet')->handle();
