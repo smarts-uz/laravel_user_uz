bc06434c6833254e49fe1764c43731d2097da137
Muslim <muslimjonqoraboyev@gmail.com>
2022-05-24 22:17:10 +0500

    ` qop ketipti

--- resources/views/vendor/voyager/report/report.blade.php
+++ resources/views/vendor/voyager/report/report.blade.php
@@ -82,7 +82,7 @@
                     {
                         "data": "",
                         render: function (data, type, row) {
-                            var details = '<button type="button" class="btn btn-primary"><a href="{{ route('show.child', '$id')}}">-></a></button>`;
+                            var details = `<button type="button" class="btn btn-primary"><a href="{{ route('show.child', '$id')}}">-></a></button>`;
                                 return details;
 
                                 
