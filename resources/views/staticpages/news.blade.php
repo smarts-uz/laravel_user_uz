95ba75ede191c868ba533fc1839a90d83233baf2
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-05-24 18:47:46 +0500

    news blade ug fix

--- resources/views/staticpages/news.blade.php
+++ resources/views/staticpages/news.blade.php
@@ -3,14 +3,15 @@
 @section('content')
 
     <div class="w-11/12 container mx-auto my-12">
+        <h1 class="text-center text-5xl font-semibold mx-6">{{__('Новости сайта')}}</h1>
         @foreach ($news as $new)
             <div class="flex lg:flex-row  flex-col gap-x-2 my-6">
                 <div class="lg:w-1/2 w-full">
                     <img class="w-full h-full rounded-lg" src="{{ asset('storage/'.$new->img) }}" alt="#">
                 </div>
                 <div class="lg:w-1/2 w-full">
-                    <h1 class="text-left text-3xl">{{$new->title}}</h1>
-                    <p class="text-left text-xl mt-3">{{$new->text}} </p>
+                    <h1 class="text-left text-3xl">{{$new->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale')}}</h1>
+                    <p class="text-left text-xl mt-3">{{$new->getTranslatedAttribute('text',Session::get('lang') , 'fallbackLocale')}}</p>
                 </div>  
             </div>
         @endforeach
