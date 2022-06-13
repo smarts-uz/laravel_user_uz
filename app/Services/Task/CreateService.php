263fcfddfd8656f3d29bc718900dc7a54c2d6ad6
Gitea <gitea@fake.local>
2022-06-13 14:15:36 +0500

    1333333

--- app/Services/Task/CreateService.php
+++ app/Services/Task/CreateService.php
@@ -5,30 +5,42 @@ namespace App\Services\Task;
 use App\Models\Address;
 use App\Models\Category;
 use App\Models\CustomFieldsValue;
+use App\Models\Task;
 use Illuminate\Support\Arr;
 
 class CreateService
 {
 
+
     public function name($request)
     {
         $current_category = Category::findOrFail($request->category_id);
-
         return view("create.name", compact('current_category'));
     }
-    public function syncCustomFields($task){
+
+    public function syncCustomFields(Task $task)
+    {
         $task->custom_field_values()->delete();
         $this->attachCustomFields($task);
 
     }
 
 
-    public function attachCustomFields($task){
+    /**
+     *
+     * Function  attachCustomFields
+     * Mazkur metod Task obyektiga, unga tegishli bo'lgan custom fieldslarni qo'shib beradi
+     * @param Task $task Task Object
+     *
+     */
+
+    public function attachCustomFields(Task $task)
+    {
         foreach ($task->category->custom_fields as $data) {
             $value = new CustomFieldsValue();
             $value->task_id = $task->id;
             $value->custom_field_id = $data->id;
-            $arr = $data->name !== null ? Arr::get(request()->all(), str_replace(' ', '_', $data->name)):null;
+            $arr = $data->name !== null ? Arr::get(request()->all(), str_replace(' ', '_', $data->name)) : null;
             $value->value = is_array($arr) ? json_encode($arr) : $arr;
             $value->save();
         }
@@ -42,24 +54,30 @@ class CreateService
         $task->addresses()->delete();
         $task->delete();
     }
-    public function attachCustomFieldsByRoute($task, $routeName, $request){
-        foreach ($task->category->custom_fields()->where('route',$routeName)->get() as $data) {
-            $value = $task->custom_field_values()->where('custom_field_id', $data->id)->first()?? new CustomFieldsValue();
+
+    public function attachCustomFieldsByRoute($task, $routeName, $request)
+    {
+        foreach ($task->category->custom_fields()->where('route', $routeName)->get() as $data) {
+            $value = $task->custom_field_values()->where('custom_field_id', $data->id)->first() ?? new CustomFieldsValue();
             $value->task_id = $task->id;
             $value->custom_field_id = $data->id;
-            $arr = $data->name !== null ? Arr::get($request->all(), str_replace(' ', '_', $data->name)):null;
+            $arr = $data->name !== null ? Arr::get($request->all(), str_replace(' ', '_', $data->name)) : null;
             $value->value = is_array($arr) ? json_encode($arr) : $arr;
             $value->save();
         }
     }
 
 
-    public function addAdditionalAddress($task, $request){
+    public function addAdditionalAddress($task, $requestAll)
+    {
         $data_inner = [];
-        $dataMain = $request->coordinates0;
-        for ($i = 0; $i < setting('site.max_address')??10; $i++) {
-            $location = Arr::get($request->all(), 'location' . $i);
-            $coordinates = Arr::get($request->all(), 'coordinates' . $i);
+        $dataMain = Arr::get($requestAll, 'coordinates0', '');
+
+        for ($i = 0; $i < setting('site.max_address') ?? 10; $i++) {
+
+            $location = Arr::get($requestAll, 'location' . $i, '');
+            $coordinates = Arr::get($requestAll, 'coordinates' . $i, '');
+
             if ($coordinates) {
                 if ($i == 0) {
                     $data_inner['default'] = 1;
