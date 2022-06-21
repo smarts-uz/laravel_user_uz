e89e6c4fc0735684d68994fda5ca5cf0daeff65c
James <smr01234585@gmail.com>
2022-06-21 14:36:13 +0500

    fixed bug

--- resources/views/task/changetask.blade.php
+++ resources/views/task/changetask.blade.php
@@ -408,11 +408,9 @@
             .use(Uppy.ImageEditor, {target: Uppy.Dashboard})
             .use(Uppy.XHRUpload, {
                 endpoint: '{{route('task.create.images.store', $task->id)}}',
-                formData: true,
                 fieldName: 'images[]',
-                headers: file => ({
-                    'X-CSRF-TOKEN': '{{csrf_token()}}'
-                }),
+                method: 'post',
+                bundle: true,
             });
 
         uppy.on('upload-success', (file, response) => {
