c5e31dd43c713f2586e40e1964983092b486dd26
James <smr01234585@gmail.com>
2022-07-29 20:36:38 +0500

    bug fix

--- app/Observers/ReviewObserver.php
+++ app/Observers/ReviewObserver.php
@@ -16,6 +16,13 @@ class ReviewObserver
     public function created(Review $review)
     {
         $user = User::find($review->user_id);
+        if (!$review->as_performer) {
+            if($review->good_bad == 1) {
+                $user->increment('review_good');
+            } else {
+                $user->increment('review_bad');
+            }
+        }
         $goods = $user->review_good;
         $bads = $user->review_bad;
         $user->review_rating = round($goods * 5 / (($goods+$bads==0) ? 1 : ($goods + $bads)));
