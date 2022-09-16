5726f188db46391401a43571924b20f4589f50c0
Faxriddin077 <faxriddinqodirov077@gmail.com>
2022-09-16 20:43:22 +0500

    elasticsearch config

---
+++ config/elastic.client.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'default' => env('ELASTIC_CONNECTION', 'default'),
+    'connections' => [
+        'default' => [
+            'hosts' => [
+                env('ELASTIC_HOST', 'localhost:9200'),
+            ],
+        ],
+    ],
+];
