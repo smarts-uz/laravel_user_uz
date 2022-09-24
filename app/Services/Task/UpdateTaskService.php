8a71f7d6b720b3297ca4c246f979c711f4afd88e
Faxriddin077 <faxriddinqodirov077@gmail.com>
2022-09-24 23:16:23 +0500

    correct phone number

--- app/Services/Task/UpdateTaskService.php
+++ app/Services/Task/UpdateTaskService.php
@@ -250,7 +250,7 @@ class UpdateTaskService
     public function verification($task, $data): JsonResponse
     {
         /** @var User $user */
-        $user = User::query()->where('phone_number', $data['phone_number'])->first();
+        $user = User::query()->where('phone_number', correctPhoneNumber($data['phone_number']))->firstOrFail();
         if ($data['sms_otp'] == $user->verify_code) {
             if (strtotime($user->verify_expiration) >= strtotime(Carbon::now())) {
                 $user->update(['is_phone_number_verified' => 1]);
