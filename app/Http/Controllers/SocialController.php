cc8b6fa10d0c08ef2d5ac637f82552e506b75387
james-msr <smr01234585@gmail.com>
2022-10-08 15:24:26 +0500

    fixing apple id

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -67,7 +67,7 @@ class SocialController extends Controller
     public function loginWithApple(Request $request)
     {
         try {
-            dd($request->all());
+            dd($request->all(), $request->token);
             $user = Socialite::driver('apple')->setScopes(['name', 'email'])->user();
             /** @var User $findUser */
             $findUser = User::query()->where('email', $user->email)->first();
