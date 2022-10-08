3bbe052e10da0237536aabb48ba9dac059c495e9
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-10-08 11:38:46 +0500

    profile defoult image bug fix

--- resources/views/components/profileFigure.blade.php
+++ resources/views/components/profileFigure.blade.php
@@ -8,9 +8,9 @@
         <div class="sm:w-1/3 pb-10 w-full">
             <img class="border border-3 border-gray-400 h-44 w-44"
                  @if ($user->avatar == Null)
-                     src='{{asset("storage/images/default.jpg")}}'
+                     src='{{asset("images/pofilevector.png")}}'
                  @else
-                     src="{{asset("storage/{$user->avatar}")}}"
+                     src="{{asset('storage/'.$user->avatar)}}"
                  @endif alt="avatar">
 
             <div class="rounded-md bg-gray-200 w-44 mt-2 py-1 border-2 border-gray-700" type="button">
