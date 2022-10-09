3177297a750115347c6b508d985445b3850fae90
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-10-09 15:39:57 +0500

    otklik bug fix

--- resources/views/detailed_task/detailed_modal.blade.php
+++ resources/views/detailed_task/detailed_modal.blade.php
@@ -170,11 +170,9 @@
 <div id="authentication-modal"
      aria-hidden="true"
      class="btn-preloader hidden overflow-x-hidden overflow-y-auto fixed h-modal md:h-full top-4 left-0 right-0 md:inset-0 z-50 justify-center items-center">
-    <div
-        class="relative w-full max-w-md px-4 h-full md:h-auto">
-        @if(Auth::check() && auth()->user()->is_phone_number_verified!=1)
-            <div
-                class="bg-white rounded-lg shadow relative dark:bg-gray-700">
+    <div class="relative w-full max-w-md h-full md:h-auto">
+        @if(Auth::check() && (int)auth()->user()->is_phone_number_verified !== 1)
+            <div class="bg-white rounded-lg shadow relative dark:bg-gray-700">
                 <div class="flex justify-end p-2">
                     <button type="button"
                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
@@ -188,13 +186,39 @@
                         </svg>
                     </button>
                 </div>
-                    <header>
-                        <h2 class="font-semibold text-3xl mb-4 text-center">{{__('Подтвердите номер телефона')}}</h2>
-                    </header>
-                    <main class="py-6">
-                       <h1 class="text-xl text-gray-900 text-center my-6">{{__('Вы не подтвердили свой номер телефона, пожалуйста, подтвердите свой номер телефона')}}</h1>
-                    </main>
-
+                <header>
+                    <h2 class="font-semibold text-3xl mb-4 text-center">{{__('Подтвердите номер телефона')}}</h2>
+                </header>
+                <main class="py-6">
+                    <h1 class="text-xl text-gray-900 text-center my-6">{{__('Вы не подтвердили свой номер телефона, пожалуйста, подтвердите свой номер телефона')}}</h1>
+                </main>
+            </div>
+        @elseif(Auth::check() && (int)auth()->user()->role_id !== 2)
+            <div class="bg-white rounded-lg shadow relative dark:bg-gray-700">
+                <div class="flex justify-end p-2">
+                    <button type="button"
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                            data-modal-toggle="authentication-modal">
+                        <svg class="w-5 h-5" fill="currentColor"
+                             viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                  clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                <header>
+                    <h2 class="font-semibold text-3xl mb-4 text-center">{{__('Вы еще не исполнитель')}}</h2>
+                </header>
+                <main class="py-6 text-center">
+                    <h1 class="text-xl text-gray-900 text-center my-6 px-4">{{__('Вы еще не являетесь руководителем Universal Services. Присоединяйтесь к списку исполнителей.')}}</h1>
+                    <a href="{{ route('profile.verificationInfo') }}">
+                        <button  class="px-6 py-2 font-sans mb-4 text-lg mt-8 font-semibold bg-yellow-500 hover:bg-yellow-600 text-white rounded-md text-lg">
+                            {{__('СТАТЬ ИСПОЛНИТЕЛЕМ')}}
+                        </button>
+                    </a>
+                </main>
             </div>
         @else
             <!-- Modal content -->
