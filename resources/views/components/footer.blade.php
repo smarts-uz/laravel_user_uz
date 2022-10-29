6d82b70a5e6640ffdb211f057f9f3b8539ef1353
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-10-29 11:15:02 +0500

    chat style bug fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -37,15 +37,15 @@
        <div class="md:col-span-1 col-span-3 flex flex-col md:mx-auto md:mx-0 lg:mt-0 mt-8">
            <div class="mb-3">
                <div><i class="fas fa-phone-alt  text-gray-300"></i><span class="font-bold text-gray-300 text-lg ml-4">{{__('Телефон')}}</span></div>
-               <h1 class="text-lg text-gray-300">+998 (91) 480 96 96</h1>
+               <span class="text-lg text-gray-300">+998 (91) 480 96 96</span>
            </div>
            <div class="my-3">
                <div><i class="far fa-envelope text-gray-300"></i><span class="font-bold text-gray-300 text-lg ml-4">{{__('Эл.Почта')}}</span></div>
-               <h1 class="text-lg text-gray-300">useruz@user.uz</h1>
+               <span class="text-lg text-gray-300">useruz@user.uz</span>
            </div>
            <div class="my-3">
                <div><i class="fas fa-map-marker-alt text-gray-300"></i><span class="font-bold text-gray-300 text-lg ml-4">{{__('Локация')}}</span></div>
-               <h1 class="text-lg text-gray-300">{{__('город Фергана')}}</h1>
+               <span class="text-lg text-gray-300">{{__('город Фергана')}}</span>
            </div>
        </div>
        <div class="md:col-span-1 col-span-3 flex flex-col md:mx-auto md:mx-0 lg:mt-0 mt-8">
--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,35 +1,35 @@
 <link rel="stylesheet" href="{{asset('css/app.css')}}">
-
-<div class="blantershow-chat fixed bottom-10 right-10 chat">
-    <div class="p-3 bg-green-700 rounded-xl cursor-pointer">
-        <i class="far fa-comments text-4xl text-white"></i>
-    </div>
-</div>
-
-<div id='whatsapp-chat' class='hide'>
-    <div class='header-chat'>
-        <div class='head-home'>
-            <div class='info-avatar'><img src='{{asset('images/1.png')}}'/></div>
-            <p><span class="whatsapp-name">Universal Services</span><br><small>{{__('Cвяжитесь с нами')}}</small></p>
+<link rel="stylesheet" href="https://zavoloklom.github.io/material-design-iconic-font/css/docs.md-iconic-font.min.css">
+<!--  CHAT  -->
+<div class="fabs">
+    <div class="chat">
+        <div class='bg-green-600 rounded-t-lg px-4 py-2 flex flex-row items-center'>
+            <div class='w-16 m-1 rounded-full bg-white p-2'><img src='{{asset('images/logo_image.png')}}'/></div>
+            <div class="ml-6 text-white">
+                <span class="text-lg">Universal Services</span><br>
+                <span class="text-xs">{{__('Cвяжитесь с нами')}}</span>
+            </div>
+        </div>
+        <div class='start-chat'>
+            <div class="flex z-10 overflow-y-auto h-96 bg-gray-50">
+                <iframe src="{{setting('site.support_chat_url')}}" frameborder="0" width="100%"></iframe>
+            </div>
         </div>
     </div>
-    <div class='start-chat'>
-        <div class="flex z-10 overflow-y-auto h-96 bg-gray-50">
-            <iframe src="{{setting('site.support_chat_url')}}" frameborder="0" width="100%"></iframe>
-        </div>
-    </div>
-    <div id='get-number'></div><a class='close-chat cursor-pointer'>×</a>
+
+    <a id="prime" class="fab_prime bg-green-500"><i class="prime zmdi zmdi-comments text-white"></i></a>
 </div>
 
 <script>
-    $(document).on("click", ".close-chat", function() {
-        $("#whatsapp-chat")
-            .addClass("hide")
-            .removeClass("show");
-    }),
-        $(document).on("click", ".blantershow-chat", function() {
-            $("#whatsapp-chat")
-                .addClass("show")
-                .removeClass("hide");
-        });
+    $('#prime').click(function() {
+        toggleFab();
+    });
+
+    function toggleFab() {
+        $('.prime').toggleClass('zmdi-comments');
+        $('.prime').toggleClass('zmdi-close');
+        $('.prime').toggleClass('is-active');
+        $('.chat').toggleClass('is-visible');
+        $('.fab').toggleClass('is-visible');
+    }
 </script>
