2c6316e559b221f8ab5d8af3bc6edbd9283f0802
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-11-08 16:35:35 +0500

    voyager form reset submit

--- resources/views/vendor/voyager/users/browse.blade.php
+++ resources/views/vendor/voyager/users/browse.blade.php
@@ -315,22 +315,50 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    <h5 class="modal-title" id="exampleModalLabel">Foydalanuvchi parolini o'zgartirish</h5>
                 </div>
                 <div class="modal-body">
-                    ...
+                    Siz foydalanuvchi parolini o'zgartirmoqchimisiz?
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary" data-dismiss="modal" data-toggle="modal" data-target="#exampleModalCenter">Ha</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Yo'q</button>
                 </div>
             </div>
         </div>
     </div>
     {{--  Reset password modal end --}}
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Foydalanuvchi parolini o'zgartirish</h5>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Saqlash</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Yopish</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('css')
