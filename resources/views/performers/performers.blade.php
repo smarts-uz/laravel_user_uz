46db6482f57ec6a52d1f24cca24225b88dfc0466
Ozodbek1405 <roziyevozodbek75@gmail.com>
2023-01-19 20:10:41 +0500

    performer search bug fix

--- resources/views/performers/performers.blade.php
+++ resources/views/performers/performers.blade.php
@@ -54,9 +54,9 @@
                     </div>
                     <div class="font-sans text-black flex ml-6 mb-3">
                         <form action="{{route('performers.service')}}" method="GET">
-                            <div class="border rounded overflow-hidden flex">
-                                <input type="search" name="search" class="px-4 py-2 focus:outline-none" placeholder="{{__('Поиск')}}..." value="{{request('search')}}">
-                                <button type="submit" class="flex items-center justify-center px-4 border-l bg-blue-400 text-white">
+                            <div class="rounded-l-lg overflow-hidden flex">
+                                <input type="search" name="search" class="px-4 py-2 focus:outline-none rounded-l-lg" placeholder="{{__('Поиск')}}..." value="{{request('search')}}">
+                                <button type="submit" class="flex items-center justify-center px-4 border-l bg-yellow-500 text-white rounded-r-lg">
                                     <svg class="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
                                 </button>
                             </div>
