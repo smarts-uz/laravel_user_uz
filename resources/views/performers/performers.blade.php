103949e4c155c789be5da28289584147216c2dcc
Ozodbek1405 <roziyevozodbek75@gmail.com>
2023-01-19 16:32:53 +0500

    bug fixed datatables

--- resources/views/performers/performers.blade.php
+++ resources/views/performers/performers.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
+
     <div class="text-sm w-full bg-gray-200 my-4 py-3">
         <p class="w-8/12 mx-auto text-gray-500 font-normal">{{__('Вы находитесь в разделе исполнителей USer.Uz')}} <br>
             {{__("Чтобы предложить работу выбранному исполнителю, нужно нажать на кнопку «Предложить задание» в его профиле.")}}</p>
@@ -48,34 +48,38 @@
             </div>
 
             <div class="lg:col-span-2 col-span-3 lg:mt-0 mt-16">
-                <div class="font-bold text-2xl mx-8 py-4">
-                    <p>{{__('Все исполнители')}}</p>
+                <div class="bg-gray-100 rounded-xl w-full sm:mx-0 mx-auto">
+                    <div class="font-bold text-2xl mx-8 py-4">
+                        <p>{{__('Все исполнители')}}</p>
+                    </div>
+                    <div class="font-sans text-black flex ml-6 mb-3">
+                        <form action="{{route('performers.service')}}" method="GET">
+                            <div class="border rounded overflow-hidden flex">
+                                <input type="search" name="search" class="px-4 py-2 focus:outline-none" placeholder="{{__('Поиск')}}..." value="{{request('search')}}">
+                                <button type="submit" class="flex items-center justify-center px-4 border-l bg-blue-400 text-white">
+                                    <svg class="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="form-check flex flex-row mx-8 pb-4">
+                        <input class="focus:outline-none  form-check-input h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-black-600 checked:border-black-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                               type="checkbox" id="online">
+                        <label class="form-check-label inline-block text-gray-800 cursor-pointer" for="online">
+                            {{__('Сейчас на сайте')}}
+                        </label>
+                    </div>
                 </div>
-
-                <table class="table yajra-datatable">
-                    <tbody>
-                    </tbody>
-                </table>
+                <div class="sortable">
+                    @foreach($users as $user)
+                        @include('performers.performers_figure')
+                    @endforeach
+                    {{ $users->links('pagination::tailwind') }}
+                </div>
             </div>
         </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(function () {
-            var table = $('.yajra-datatable').DataTable({
-                processing: true,
-                serverSide: true,
-                ajax: "{{ route('performers.list') }}",
-                columns: [
-                    {data: 'user_images'},
-                    {data: 'user_information'},
-                ]
-            });
-        });
-    </script>
+
     @include('performers.performers_modal')
     @include('performers.performer_script')
 @endsection
-
