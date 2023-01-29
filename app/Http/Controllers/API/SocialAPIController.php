520371bbc2cee285d5cfcfca307d05b48c49cc69
Bexruz999 <bexruzfatullayev999@gmail.com>
2023-01-29 21:50:58 +0500

    deleted user email withTrashed

--- app/Http/Controllers/API/SocialAPIController.php
+++ app/Http/Controllers/API/SocialAPIController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Api\SocialRequest;
 use App\Http\Resources\PerformerIndexResource;
 use App\Models\User;
 use App\Models\WalletBalance;
+use App\Services\User\SocialService;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -53,70 +54,19 @@ class SocialAPIController extends Controller
      */
     public function login(SocialRequest $request): \Illuminate\Http\JsonResponse
     {
-        try {
-            $data = $request->validated();
-            switch ((int)$data['type']){
-                case 0 :
-                    $provider = 'google';
-                    break;
-                case 1 :
-                    $provider = 'facebook';
-                    break;
-                default :
-                    $provider = 'apple';
-                    break;
-            }
-
-            $providerUser = Socialite::driver($provider)->userFromToken($data['access_token']);
-
-            $query = User::query()
-                ->where($provider . '_id', $providerUser->id);
-
-            if ($providerUser->email !== null) {
-                $query->orWhere('email', $providerUser->email);
-            }
-
-            $user = $query->first();
-
-            // if there is no record with these data, create a new user
-            if ($user === null) {
-                $user = User::query()->create([
-                    $provider . '_id' => $providerUser->id,
-                    'name' => $providerUser->name,
-                    'email' => $providerUser->email,
-                    'is_email_verified' => 1,
-                    'avatar' => $provider != 'apple' ? self::get_avatar($providerUser) : null
-                ]);
-                $wallBal = new WalletBalance();
-                $wallBal->balance = setting('admin.bonus');
-                $wallBal->user_id = $user->id;
-                $wallBal->save();
-            }
-            if (!$user->isActive()) {
-                return response()->json([
-                    'success' => false,
-                    'message' => __('Аккаунт отключен'),
-                ]);
-            }
-
-            $user->update([$provider . '_id' => $providerUser->id]);
-
-            // create a token for the user, so they can login
-            Auth::login($user);
-            $accessToken = $user->createToken('authToken')->accessToken;
-            // return the token for usage
-            return response()->json([
-                'user' => new PerformerIndexResource(auth()->user()),
-                'access_token' => $accessToken,
-                'socialpas' => $user->has_password
-            ]);
-        } catch (Exception $e) {
-            return response()->json([
-                'success' => false,
-                'data' => $e->getMessage(),
-                'message' => "Record not found"
-            ]);
+        $data = $request->validated();
+        switch ((int)$data['type']){
+            case 0 :
+                $provider = 'google';
+                break;
+            case 1 :
+                $provider = 'facebook';
+                break;
+            default :
+                $provider = 'apple';
+                break;
         }
+        return SocialService::login($provider, $data['access_token']);
     }
 
     // login with google
@@ -153,13 +103,5 @@ class SocialAPIController extends Controller
             'provider_id' => $user->id,
             'avatar' => $user->avatar
         ]);
-    }
-
-    private static function get_avatar($user)
-    {
-        $fileContents = file_get_contents($user->getAvatar());
-        File::put(public_path() . '/storage/user-avatar/' . $user->getId() . ".jpg", $fileContents);
-        $picture = 'user-avatar/' . $user->getId() . ".jpg";
-        return $picture;
     }
 }
---
+++ app/Services/User/SocialService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Http\Resources\PerformerIndexResource;
+use App\Models\User;
+use App\Models\WalletBalance;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialService
+{
+    public static function login($provider, $token) {
+        try {
+            $providerUser = Socialite::driver($provider)->userFromToken($token);
+
+            $query = User::query()
+                ->where($provider . '_id', '=', $providerUser->id);
+
+            if ($providerUser->email !== null) {
+                $query->orWhere('email', '=', $providerUser->email);
+            }
+
+            $user = $query->withTrashed()->first();
+
+            // if there is no record with these data, create a new user
+            if ($user === null) {
+                $user = User::query()->create([
+                    $provider . '_id' => $providerUser->id,
+                    'name' => $providerUser->name,
+                    'email' => $providerUser->email,
+                    'is_email_verified' => 1,
+                    'avatar' => $provider !== 'apple' ? self::get_avatar($providerUser) : null
+                ]);
+                $wallBal = new WalletBalance();
+                $wallBal->balance = setting('admin.bonus');
+                $wallBal->user_id = $user->id;
+                $wallBal->save();
+            }
+            if (!$user->isActive()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => __('Аккаунт отключен'),
+                ]);
+            }
+
+            $user->update([$provider . '_id' => $providerUser->id]);
+
+            // create a token for the user, so they can login
+            Auth::login($user);
+            $accessToken = $user->createToken('authToken')->accessToken;
+            // return the token for usage
+            return response()->json([
+                'user' => new PerformerIndexResource(auth()->user()),
+                'access_token' => $accessToken,
+                'socialpas' => $user->has_password
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'data' => $e->getMessage(),
+                'message' => "Record not found"
+            ]);
+        }
+    }
+    private static function get_avatar($user)
+    {
+        $fileContents = file_get_contents($user->getAvatar());
+        File::put(public_path() . '/storage/user-avatar/' . $user->getId() . ".jpg", $fileContents);
+        return 'user-avatar/' . $user->getId() . ".jpg";
+    }
+
+}
