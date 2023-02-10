6215453315097185bda3e925ff90aa776f8bfebf
anthony-akbar <mamadjanovakbar@gmail.com>
2023-02-10 16:00:06 +0500

    PerformerAPIController to service completed

--- app/Services/PerformersService.php
+++ app/Services/PerformersService.php
@@ -34,7 +34,7 @@ class PerformersService
      */
     public function service($authId, $search, ?string $lang = 'uz'): PerformerServiceItem
     {
-        $category = Cache::remember('category_' . $lang, now()->addMinute(180), function () use($lang) {
+        $category = Cache::remember('category_' . $lang, now()->addMinute(180), function () use ($lang) {
             return Category::withTranslations($lang)->orderBy("order")->get();
         });
 
@@ -42,12 +42,12 @@ class PerformersService
         $item->tasks = Task::query()->where('user_id', $authId)
             ->whereIn('status', [Task::STATUS_OPEN, Task::STATUS_RESPONSE])->orderBy('created_at', 'DESC')
             ->get();
-        $item->categories =  collect($category)->where('parent_id', null)->all();
+        $item->categories = collect($category)->where('parent_id', null)->all();
         $item->categories2 = collect($category)->where('parent_id', '!=', null)->all();
         $item->users = User::query()
             ->where('role_id', User::ROLE_PERFORMER)
             ->where('name', 'LIKE', "%{$search}%")
-            ->WhereNot('id',\auth()->id())
+            ->WhereNot('id', \auth()->id())
             ->orderByDesc('review_rating')
             ->orderbyRaw('(review_good - review_bad) DESC')->paginate(50);
 
@@ -81,8 +81,8 @@ class PerformersService
         $item->badReviews = $user->badReviews()->whereHas('task')->whereHas('user')->latest()->get();
         $item->task_count = Task::query()->where('user_id', Auth::id())
             ->whereIn('status', [Task::STATUS_OPEN, Task::STATUS_RESPONSE, Task::STATUS_IN_PROGRESS, Task::STATUS_COMPLETE, Task::STATUS_NOT_COMPLETED, Task::STATUS_CANCELLED])->get();
-        $user_categories = UserCategory::query()->where('user_id',$user->id)->pluck('category_id')->toArray();
-        $item->user_category = Category::query()->whereIn('id',$user_categories)->get();
+        $user_categories = UserCategory::query()->where('user_id', $user->id)->pluck('category_id')->toArray();
+        $item->user_category = Category::query()->whereIn('id', $user_categories)->get();
         $value = Carbon::parse($user->created_at)->locale(getLocale());
         $day = $value == now()->toDateTimeString() ? "Bugun" : "$value->day-$value->monthName";
         $item->created = "$day  {$value->year}";
@@ -98,18 +98,18 @@ class PerformersService
      * @param $cf_id
      * @return PerformerPrefItem
      */
-    public function perf_ajax($authId,$search,$cf_id): PerformerPrefItem
+    public function perf_ajax($authId, $search, $cf_id): PerformerPrefItem
     {
         $item = new PerformerPrefItem();
         $item->categories = Category::query()->where('parent_id', null)
             ->select('id', 'name', 'slug')->orderBy("order", "asc")->get();
         $item->categories2 = Category::query()->where('parent_id', '<>', null)
             ->select('id', 'parent_id', 'name')->orderBy("order", "asc")->get();
-        $item->user_categories = UserCategory::query()->where('category_id',$cf_id)->pluck('user_id')->toArray();
+        $item->user_categories = UserCategory::query()->where('category_id', $cf_id)->pluck('user_id')->toArray();
         $item->users = User::query()
             ->where('role_id', User::ROLE_PERFORMER)
             ->where('name', 'LIKE', "%{$search}%")
-            ->WhereNot('id',\auth()->id())
+            ->WhereNot('id', \auth()->id())
             ->whereIn('id', $item->user_categories)
             ->orderByDesc('review_rating')
             ->orderbyRaw('(review_good - review_bad) DESC')->paginate(50);
@@ -133,53 +133,49 @@ class PerformersService
     {
         $performers = User::query()
             ->where('role_id', User::ROLE_PERFORMER)
-            ->WhereNot('id',\auth()->id());
+            ->WhereNot('id', \auth()->id());
 
         if (isset($data['categories'])) {
-            $categories = is_array($data['categories'])?$data['categories']:json_decode($data['categories']);
-            $childCategories = \App\Models\Category::query()->whereIn('parent_id',$categories)->pluck('id')->toArray();
+            $categories = is_array($data['categories']) ? $data['categories'] : json_decode($data['categories']);
+            $childCategories = \App\Models\Category::query()->whereIn('parent_id', $categories)->pluck('id')->toArray();
             $allCategories = array_unique(array_merge($categories, $childCategories));
-            $user_categories = UserCategory::query()->whereIn('category_id',$allCategories)->pluck('user_id')->toArray();
+            $user_categories = UserCategory::query()->whereIn('category_id', $allCategories)->pluck('user_id')->toArray();
             $performers = $performers->whereIn('id', $user_categories);
         }
         if (isset($data['child_categories'])) {
-            $categories = is_array($data['child_categories'])?$data['child_categories']:json_decode($data['child_categories']);
-            $user_categories = UserCategory::query()->whereIn('category_id',$categories)->pluck('user_id')->toArray();
+            $categories = is_array($data['child_categories']) ? $data['child_categories'] : json_decode($data['child_categories']);
+            $user_categories = UserCategory::query()->whereIn('category_id', $categories)->pluck('user_id')->toArray();
             $performers = $performers->whereIn('id', $user_categories);
         }
 
-        if (isset($data['online']))
-        {
+        if (isset($data['online'])) {
             $date = Carbon::now()->subMinutes(2)->toDateTimeString();
-            $performers = $performers->where('last_seen', ">=",$date);
+            $performers = $performers->where('last_seen', ">=", $date);
         }
 
-        if(isset($data['review'], $data['desc'])){
+        if (isset($data['review'], $data['desc'])) {
             $performers = $performers
                 ->orderByDesc('review_rating')
                 ->orderByRaw('(review_good - review_bad) DESC');
         }
 
-        if(isset($data['review'], $data['asc'])){
+        if (isset($data['review'], $data['asc'])) {
             $performers = $performers
                 ->orderBy('review_rating')
                 ->orderByRaw('(review_good - review_bad) DESC');
         }
 
-        if (isset($data['alphabet'], $data['desc']))
-        {
-            $performers = $performers->orderBy('name','desc');
+        if (isset($data['alphabet'], $data['desc'])) {
+            $performers = $performers->orderBy('name', 'desc');
         }
 
-        if (isset($data['alphabet'], $data['asc']))
-        {
+        if (isset($data['alphabet'], $data['asc'])) {
             $performers = $performers->orderBy('name');
         }
 
-        if (isset($data['search']))
-        {
+        if (isset($data['search'])) {
             $search = $data['search'];
-            $performers = $performers->where('name','like',"%$search%");
+            $performers = $performers->where('name', 'like', "%$search%");
         }
 
         return PerformerIndexResource::collection($performers->paginate(20));
@@ -203,7 +199,7 @@ class PerformersService
             $users_id = $session->pull('given_id');
             /** @var User $performer */
             $performer = User::query()->find($users_id);
-            $text_url = route("searchTask.task",$task_id);
+            $text_url = route("searchTask.task", $task_id);
             $message = __('Вам предложили новое задание task_name №task_id от заказчика task_user', [
                 'task_name' => $text_url, 'task_id' => $task_id, 'task_user' => $task_name->user?->name
             ]);
@@ -244,7 +240,7 @@ class PerformersService
         /** @var User $performer */
         $performer = User::query()->findOrFail($performer_id);
         $locale = cacheLang($performer->id);
-        $text_url = route("searchTask.task",$task_id);
+        $text_url = route("searchTask.task", $task_id);
         $message = __('Вам предложили новое задание task_name №task_id от заказчика task_user', [
             'task_name' => $text_url, 'task_id' => $task->id, 'task_user' => $task->user?->name
         ], $locale);
@@ -290,4 +286,65 @@ class PerformersService
         }
         return false;
     }
+
+    public function performers_image($category_id)
+    {
+        $user_cat = UserCategory::query()->where('category_id', $category_id)->pluck('user_id')->toArray();
+        $user_image = User::query()->whereIn('id', $user_cat)->take(3)->get();
+        $images = [];
+        foreach ($user_image as $image) {
+            $images[] = asset('storage/' . $image->avatar);
+        }
+        switch (count($user_image)) {
+            case(0):
+                $images[0] = asset('images/Rectangle2.png');
+                $images[1] = asset('images/Ellipse1.png');
+                $images[2] = asset('images/performer4.jpg');
+                break;
+            case(1):
+                $images[1] = asset('images/performer1.jpg');
+                $images[2] = asset('images/performer2.jpg');
+                break;
+            case(2):
+                $images[2] = asset('images/Rectangle4.png');
+                break;
+            default:
+        }
+        return $images;
+    }
+
+    public function reviews($from, $type)
+    {
+        $reviews = Review::query()
+            ->whereHas('task')->whereHas('user')
+            ->where('user_id', auth()->id())
+            ->fromUserType($from)
+            ->type($type)
+            ->get();
+
+        return $reviews;
+    }
+
+    public function becomePerformerEmailPhone($user, $data)
+    {
+        if ($data['phone_number'] !== $user->phone_number) {
+            $user->phone_number = $data['phone_number'];
+            $user->is_phone_number_verified = 0;
+        }
+        if ($data['email'] !== $user->email) {
+            $user->email = $data['email'];
+            $user->is_email_verified = 0;
+        }
+        $user->save();
+        return response()->json(['success' => 'true', 'message' => __('Успешно обновлено')]);
+    }
+
+    public function becomePerformerData($user, $data)
+    {
+        $data['born_date'] = Carbon::parse($data['born_date'])->format('Y-m-d');
+        $user->update($data);
+
+        return response()->json(['success' => 'true', 'message' => __('Успешно обновлено')]);
+    }
+
 }
