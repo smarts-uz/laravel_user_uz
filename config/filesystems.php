ccd6f74425a59b969a833b9341f81e82388d2421
Ozodbek1405 <roziyevozodbek75@gmail.com>
2023-02-25 10:10:46 +0500

    qwfew

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,13 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'voyager' => [
+            'driver' => 'local',
+            'root' => public_path('/public'),
+            'url' => env('APP_URL').'/public',
+            'visibility' => 'public',
+        ],
+
     ],
 
     /*
