ab829dd21d01daead2e32a29eb6ca68bd3057bf4
Ozodbek1405 <roziyevozodbek75@gmail.com>
2023-04-03 10:16:24 +0500

    addresses table add soft delete

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property $id
@@ -19,7 +20,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $guarded = [];
 
